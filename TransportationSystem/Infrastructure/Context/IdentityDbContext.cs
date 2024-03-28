361413082ca562dcd5a363efdea336974563270f
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2024-03-28 17:13:14 +0200

    init

---
+++ TransportationSystem/Infrastructure/Context/IdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Model.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class IdentityDbContext : IdentityDbContext<BusStopManger>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ TransportationSystem/Transportation Api/Helpers/DI/ContextDI.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Model.Identity;
+
+namespace Transportation_Api.Helpers.DI
+{
+    public static class ContextDI
+    {
+        public static IServiceCollection AddContextDI(this IServiceCollection services, IConfiguration _configuration)
+        {
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<BusStopManger, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("System"));
+            });
+
+            services.AddDbContext<IdentityDbContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("Identity"));
+            });
+
+            return services;
+        }
+    }
+}
